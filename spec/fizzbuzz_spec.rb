e58820fd75f81851eee7899eec40a5dc32f13b22
Timothy Robinson <timmyrobby@gmail.com>
2017-04-03 15:36:34 +0100

    adds second test

---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,12 @@
+require 'fizzbuzz'
+
+describe 'fizzbuzz' do
+
+ it 'returns "fizz" if passed 3' do
+   expect(fizzbuzz(3)).to eq "fizz"
+ end
+
+ it 'returns "buzz" if passed 5' do
+   expect(fizzbuzz(5)).to eq "buzz"
+ end
+end
